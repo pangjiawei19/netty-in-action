73d14dff85573a3f12a5b2a1a4c6a38069b568a6
pangjiawei <pangjiawei@magictavern.com>
2023-05-22 22:18:38 +0800

    feat: don't write when channel is unwritable

--- src/main/java/io/netty/example/study/server/handler/OrderServerProcessHandler.java
+++ src/main/java/io/netty/example/study/server/handler/OrderServerProcessHandler.java
@@ -6,13 +6,15 @@ import io.netty.example.study.common.Operation;
 import io.netty.example.study.common.OperationResult;
 import io.netty.example.study.common.RequestMessage;
 import io.netty.example.study.common.ResponseMessage;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author pangjiawei
  */
+@Slf4j
 public class OrderServerProcessHandler extends SimpleChannelInboundHandler<RequestMessage> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, RequestMessage requestMessage) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, RequestMessage requestMessage) {
         Operation operation = requestMessage.getMessageBody();
         OperationResult operationResult = operation.execute();
 
@@ -20,6 +22,10 @@ public class OrderServerProcessHandler extends SimpleChannelInboundHandler<Reque
         responseMessage.setMessageBody(operationResult);
         responseMessage.setMessageHeader(requestMessage.getMessageHeader());
 
-        ctx.writeAndFlush(responseMessage);
+        if (ctx.channel().isActive() && ctx.channel().isWritable()) {
+            ctx.writeAndFlush(responseMessage);
+        } else {
+            log.error("message dropped");
+        }
     }
 }
